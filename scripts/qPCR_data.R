ebaadc419be794c41c1099c8ca723a5faef6dd28
Priya Shukla <prishukla.sci@gmail.com>
2023-04-03 18:31:48 -0700

    analyzing qPCR data

--- scripts/qPCR_data.R
+++ scripts/qPCR_data.R
@@ -2,8 +2,7 @@
 
 ## load libraries
 library(tidyverse)
-library(scales)
-install.packages("scales") # to access break formatting functions
+library(scales) # to access break formatting functions for log scale
 
 ########## 2020 Cohort OsHV-1 Data ==============
 OsHV1 <- read_csv("data/qPCR/qPCR_runs.csv")
@@ -31,16 +30,21 @@ OsHV1 %>%
             SD_Copies = sd(log_transform),
             SE_Copies = SD_Copies/sqrt(n()))
 
-ggplot(OsHV1, aes(x = Site, y = log_transform, color = Year_Sampled)) +
+OsHV1 %>%
+filter(Cohort == "2020" ) %>% 
+ggplot(aes(x = Site, y = log_transform, color = Year_Sampled)) +
+  geom_point() +
+  scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
+                labels = trans_format("log10", math_format(10^.x))) +
+  theme_classic()
+
+
+#### 2020 v 2022 Cohort Stats by Year =====
+OsHV1 %>%
+  filter(Sample_Date == "27-Aug-22") %>% 
+  ggplot(aes(x = Bag, y = log_transform, color = Bag)) +
   geom_point() +
   scale_y_log10(breaks = trans_breaks("log10", function(x) 10^x),
                 labels = trans_format("log10", math_format(10^.x))) +
   theme_classic()
   
-
-#### 2020 Cohort Stats by Year ===== 
-OsHV1 %>%
-  group_by(Year_Sampled, Site) %>% 
-  summarize(Mean_Copies = mean(log_transform),
-            SD_Copies = sd(log_transform),
-            SE_Copies = SD_Copies/sqrt(n()))
